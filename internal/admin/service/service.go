d59624d0848b4b2335971641428cd829f9d4e4f2
Lev Ustimenko <Leva3175@gmail.com>
2024-07-16 12:55:33 +0300

    Рефактор service и конфига postman

--- internal/admin/service/service.go
+++ internal/admin/service/service.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/kyogai2281337/cns_eljur/internal/admin/structures"
 	"github.com/kyogai2281337/cns_eljur/pkg/server"
 	"github.com/kyogai2281337/cns_eljur/pkg/sql/model"
+	"github.com/sirupsen/logrus"
 )
 
 type AdminPanelController struct {
@@ -30,12 +32,19 @@ func (c *AdminPanelController) Authentication() fiber.Handler {
 func (c *AdminPanelController) GetObj(req *fiber.Ctx) error {
 	request := &structures.GetObjRequest{}
 	if err := req.BodyParser(request); err != nil {
+		logrus.WithError(err).Error("Failed to parse req Body")
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
+	logrus.WithFields(logrus.Fields{
+		"tableName": request.TableName,
+		"id":        request.Id,
+	}).Info("GetObj request received")
+
 	switch request.TableName {
 	case "users":
 		dbRequest, err := c.Server.Store.User().Find(request.Id)
 		if err != nil {
+			logrus.WithError(err).Error("Failed to find user")
 			return err
 		}
 		response := &structures.GetUserResponse{
@@ -47,22 +56,30 @@ func (c *AdminPanelController) GetObj(req *fiber.Ctx) error {
 			IsActive:  dbRequest.IsActive,
 		}
 		return req.JSON(response)
+	default:
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid table name")
 	}
-
-	return nil
 }
 
 func (c *AdminPanelController) GetList(req *fiber.Ctx) error {
 	request := &structures.GetListRequest{}
 	if err := req.BodyParser(request); err != nil {
+		logrus.WithError(err).Error("Failed to parse request body")
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
+
+	logrus.WithFields(logrus.Fields{
+		"tableName": request.TableName,
+		"page":      request.Page,
+		"limit":     request.Limit,
+	}).Info("GetList request received")
 
 	Table := request.TableName
 	switch Table {
 	case "users":
 		users, err := c.Server.Store.User().GetUserList(request.Page, request.Limit)
 		if err != nil { // Todo: эти структуры юзеров не отдавать(отдавать структуру которая в тудухе)
+			logrus.WithError(err).Error("Failed to get user list")
 			return fiber.NewError(fiber.StatusBadRequest, err.Error())
 		}
 		var response structures.GetListResponse
@@ -74,6 +91,7 @@ func (c *AdminPanelController) GetList(req *fiber.Ctx) error {
 	case "roles":
 		roles, err := c.Server.Store.Role().GetRoleList(request.Page, request.Limit)
 		if err != nil {
+			logrus.WithError(err).Error("Failed to get role list")
 			return fiber.NewError(fiber.StatusBadRequest, err.Error())
 		}
 		return req.JSON(roles)
@@ -83,21 +101,41 @@ func (c *AdminPanelController) GetList(req *fiber.Ctx) error {
 }
 
 func (c *AdminPanelController) GetTables(req *fiber.Ctx) error {
+	logrus.Info("GetTables request received")
 	response := structures.GetTablesResponse{Tables: c.Server.Store.GetTables()}
 	return req.JSON(response)
 }
 func (c *AdminPanelController) SetObj(req *fiber.Ctx) error {
 	request := &structures.SetObj{}
 	if err := req.BodyParser(request); err != nil {
+		logrus.WithError(err).Error("Failed to parse request body")
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
+
+	logrus.WithFields(logrus.Fields{
+		"tableName": request.TableName,
+		"table":     request.Table,
+	}).Info("SetObj request received")
+
 	switch request.TableName {
 	case "users":
-		if err := c.Server.Store.User().UpdateUser(request.Table.(*model.User)); err != nil {
+		var user model.User
+		tabledata, err := json.Marshal(request.Table)
+		if err != nil {
+			logrus.WithError(err).Error("Failed to marshal table data")
+			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		}
+		if err := json.Unmarshal(tabledata, &user); err != nil {
+			logrus.WithError(err).Error("Failed to unmarshal table data into user object")
+			return fiber.NewError(fiber.StatusBadRequest, "Invalid user object")
+		}
+		if err := c.Server.Store.User().UpdateUser(&user); err != nil {
+			logrus.WithError(err).Error("Failed to update user")
 			return fiber.NewError(fiber.StatusBadRequest, err.Error())
 		}
-		dbRequest, err := c.Server.Store.User().Find(request.Table.(*model.User).ID)
+		dbRequest, err := c.Server.Store.User().Find(user.ID)
 		if err != nil {
+			logrus.WithError(err).Error("Failed to find updated user")
 			return err
 		}
 		response := &structures.GetUserResponse{
@@ -108,8 +146,13 @@ func (c *AdminPanelController) SetObj(req *fiber.Ctx) error {
 			Role:      dbRequest.Role,
 			IsActive:  dbRequest.IsActive,
 		}
+		logrus.WithFields(logrus.Fields{
+			"userID":    response.ID,
+			"userEmail": response.Email,
+		}).Info("User updated successfully")
 		return req.JSON(response)
 	default:
-		return fiber.NewError(fiber.StatusBadRequest, "Invalid request")
+		logrus.Error("Invalid table name")
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid table name")
 	}
 }
